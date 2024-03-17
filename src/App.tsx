ff493fe9a0a0496a3b6e5e9b6c18744d9577e11e
jangminseo <philanthroo@naver.com>
2024-03-17 12:52:29 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './page/mainpage/Mainpage';
+
+function App() {
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' Component={MainPage}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/page/mainpage/Mainpage.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+
+
+export default function MainPage() {
+  return (
+    <div className="mainpage_container">
+      hi
+    </div>
+  )
+}
